7d91ddb491973b4e28c290bd9663fb4236d9a7be
Nino Uzelac <uzelac.nino@gmail.com>
2018-03-18 21:47:10 +0000

    Add function for checking if arr is sorted

--- randomsort.adb
+++ randomsort.adb
@@ -1,13 +1,37 @@
 with Gnat.Io; use Gnat.Io;
+
 procedure RandomSort is
-    A: array(1..5) of Integer;
+		type IntArray is array(Integer range <>) of Integer;
+
+		function IsSorted(A: in IntArray) return Boolean is
+		begin
+			for I in A'First .. A'Last - 1 loop
+				if A(I) > A(I+1) then
+				return False;
+				end if;
+			end loop;
+
+			return True;
+		end;
+
+    A: IntArray(1..5);
     I: Integer;
+    B: Boolean;
 begin
     for I in 1..5 loop
         Get(A(I));
     end loop;
-
     for I in reverse A'Range loop
         Put(A(I));
     end loop;
+    New_line;
+
+    B := IsSorted(A);
+
+    if B then
+    	Put("True");
+    else
+    	Put("False");
+    end if;
+
 end RandomSort;
